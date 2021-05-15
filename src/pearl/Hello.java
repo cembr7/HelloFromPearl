ea58a5b3dd4f29491b381cd889b4ee41e9a7777e
Swenson <swensonmbailey@yahoo.com>
2021-05-15 09:35:10 -0500

    adds message from swenson

---
+++ src/pearl/Hello.java
@@ -0,0 +1,9 @@
+package pearl;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("pearl.Hello, my name is Pearl");
+        System.out.println("Hey Pearl, my name is Swenson");
+
+    }
+}
