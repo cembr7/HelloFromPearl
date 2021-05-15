d57df1e6e6609611dc4338b8e49e679bd63a3d3d
cembr7 <83994596+cembr7@users.noreply.github.com>
2021-05-15 11:59:54 -0700

    Update Hello.java
    stretch

--- src/pearl/Hello.java
+++ src/pearl/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("pearl.Hello, my name is Pearl");
         System.out.println("Hey Pearl, my name is Swenson");
+        System.out.println("Stretch Challange");
 
     }
 }
